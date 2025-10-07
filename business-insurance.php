dcc9e370b100eb81ef7afadcc45789992f626833
dev-orchid <dev@orchidsw.com>
2025-10-07 15:00:27 +0530

    first commit to staging1.3

--- business-insurance.php
+++ business-insurance.php
@@ -1,6 +1,6 @@
 <?php
 $queryParams = !empty($_SERVER['QUERY_STRING']) ? "&" . $_SERVER['QUERY_STRING'] : '';
-// Extract the 'id2' parameter value tesy
+// Extract the 'id2' parameter value test
 //$id2 = $_REQUEST['id2'] ?? '';
 // Split the id2 string into parts
 //$parts = explode('--', $id2);
